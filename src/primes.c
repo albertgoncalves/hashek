60f50bddecf0e41170b9ff6b2e3e8b90f36b0e96
Albert Goncalves <goncalves.albert@gmail.com>
2020-04-19 20:37:27 -0400

    clean-up primes.c

--- src/primes.c
+++ src/primes.c
@@ -2,25 +2,26 @@
 
 #include "primes.h"
 
-bool is_prime(size_t x) {
-    if (x < 2) {
+bool is_prime(size_t value) {
+    if (value < 2) {
         return false;
-    } else if (x < 4) {
+    } else if (value < 4) {
         return true;
-    } else if ((x % 2) == 0) {
+    } else if ((value % 2) == 0) {
         return false;
     }
-    for (size_t i = 3; (float)i <= floorf(sqrtf((float)x)); i += 2) {
-        if ((x % i) == 0) {
+    float v = floorf(sqrtf((float)value));
+    for (size_t i = 3; i <= (size_t)v; i += 2) {
+        if ((value % i) == 0) {
             return false;
         }
     }
     return true;
 }
 
-size_t next_prime(size_t x) {
-    while (!is_prime(x)) {
-        ++x;
+size_t next_prime(size_t value) {
+    while (!is_prime(value)) {
+        ++value;
     }
-    return x;
+    return value;
 }
