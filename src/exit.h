2e4a7e21eac23872a0cf251dbf3dd3e4c704b09a
albertgoncalves <goncalves.albert@gmail.com>
2020-04-20 18:53:48 -0400

    breakout exit macro

---
+++ src/exit.h
@@ -0,0 +1,27 @@
+#ifndef __EXIT_H__
+#define __EXIT_H__
+
+static const char* BOLD_PURPLE    = "\033[1;35m";
+static const char* BOLD           = "\033[1m";
+static const char* BOLD_UNDERLINE = "\033[1;4m";
+static const char* CLOSE          = "\033[0m";
+
+#define EXIT_IF(condition)                                  \
+    if (condition) {                                        \
+        fprintf(stderr,                                     \
+                "<%s%s%s>.%s%s%s() @%sline %d%s: %sExit%s", \
+                BOLD,                                       \
+                __FILE__,                                   \
+                CLOSE,                                      \
+                BOLD,                                       \
+                __func__,                                   \
+                CLOSE,                                      \
+                BOLD_UNDERLINE,                             \
+                __LINE__,                                   \
+                CLOSE,                                      \
+                BOLD_PURPLE,                                \
+                CLOSE);                                     \
+        exit(1);                                            \
+    }
+
+#endif
--- src/hash_table.c
+++ src/hash_table.c
@@ -3,33 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "exit.h"
 #include "hash_table.h"
 #include "primes.h"
 
-static const size_t RESIZE_UP = 64;
-
-static const char* BOLD_PURPLE    = "\033[1;35m";
-static const char* BOLD           = "\033[1m";
-static const char* BOLD_UNDERLINE = "\033[1;4m";
-static const char* CLOSE          = "\033[0m";
-
-#define EXIT_IF(condition)                                  \
-    if (condition) {                                        \
-        fprintf(stderr,                                     \
-                "<%s%s%s>.%s%s%s() @%sline %d%s: %sExit%s", \
-                BOLD,                                       \
-                __FILE__,                                   \
-                CLOSE,                                      \
-                BOLD,                                       \
-                __func__,                                   \
-                CLOSE,                                      \
-                BOLD_UNDERLINE,                             \
-                __LINE__,                                   \
-                CLOSE,                                      \
-                BOLD_PURPLE,                                \
-                CLOSE);                                     \
-        exit(1);                                            \
-    }
+static const size_t RESIZE_UP = 70;
 
 static ht_item_t NULL_ITEM = {NULL, NULL};
 
